a2c5d7a5c408933e7f67a40de32b8b2ca2293f7f
david-why <david_why@outlook.com>
2024-05-04 15:29:21 +0800

    use cx_Freeze instead of pyinstaller

--- apcalt_python/_entrypoint.py
+++ apcalt_python/_entrypoint.py
@@ -3,13 +3,30 @@ import sys
 
 from uvicorn.main import main
 
-os.environ['FLASK_SESSION_TYPE'] = 'filesystem'
+try:
+    import certifi
+    import ssl
+
+    _create_default_context = ssl.create_default_context
+
+    def create_default_context(*args, **kwargs):
+        ctx = _create_default_context(*args, **kwargs)
+        ctx.load_verify_locations(certifi.where())
+        return ctx
+
+    ssl.create_default_context = create_default_context
+except ImportError:
+    pass
+
+os.environ.setdefault('FLASK_SESSION_TYPE', 'filesystem')
 sys.argv[1:] = ['apcalt_python.__main__:app', '--host', '0.0.0.0', '--port', '8052']
 
 from apcalt_python.__main__ import app
 
+
 @app.before_serving
 def before_serving():
     print('**** APCAlt has started! Please visit: http://localhost:8052 ****')
+
 
 main()
